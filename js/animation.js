71e20fc6aa84b0c9eaa531fafb4f1f6faa7993bc
Brennan Boman <brennanboman@Brennans-MacBook-Air.local>
2017-02-04 12:07:11 -0700

    got :hover to work

--- js/animation.js
+++ js/animation.js
@@ -29,8 +29,50 @@
    $('.tweet-compose').height(25)
  });*/
 
- $("#tweet-submit").on('click', function() {
+//doesn't work
+/* $("#tweet-submit").on('click', function() {
      $(".tweet-compose").prependTo("#stream");
      $(".avatar").prependTo("#stream")
      $("#profile-summary").prependTo("#stream")
- });
+ });*/
+
+
+//below lets me submit tweet
+ $('#tweet-submit').on('click', function() {
+    var tweetText = $('.tweet-compose').val();
+    var newBrenTweet = $('.tweet').first().clone();
+
+   newBrenTweet.find('.avatar').attr('src', 'https://pbs.twimg.com/profile_images/1610424916/image.jpg');
+    newBrenTweet.find('.fullname').html('Brennan Boman');
+    newBrenTweet.find('.username').html('@bboman');
+    newBrenTweet.find('.tweet-text').html(tweetText);
+    newBrenTweet.find('.num-retweets').html(0);
+    newBrenTweet.find('.num-favorites').html(0);
+    newBrenTweet.find('tweet-compose').html('reply to @btagge');
+    newBrenTweet.find('.users-interact').html('<div></div>');
+
+    $('#stream').prepend(newBrenTweet);
+
+   $('#new-tweetBox').val('');
+    $('#char-count').html(lengthAllowed);
+    $('#tweet-controls').hide();
+    $('#new-tweetBox').css('height', '30px');
+
+   $('.tweet').on('mouseover',function() {
+        $(this).find('.tweet-actions').show();
+    });
+
+   $('.tweet').on('mouseout', function() {
+        $(this).find('.tweet-actions').hide();
+    });
+
+   $('.tweet').on('click', function() {
+        $(this).find('.stats').toggle('fast');
+    });
+  });
+
+//trying to make tweet-actions only show when hovering over
+/*$('.tweet-text').hover(function(){
+  $('.tweet-actions').css('visibility', 'visible')
+});  didn't work */
+//will try to do with css *update -- got it with CSS
